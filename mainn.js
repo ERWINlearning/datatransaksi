77e01f7471cabf159d352179a5e6932453a86510
ERWINlearning <139995811+ERWINlearning@users.noreply.github.com>
2023-12-28 15:29:54 +0700

    Add files via upload

--- mainn.js
+++ mainn.js
@@ -1,8 +1,7 @@
 function inputdata() {
-    var a = document.forms['data']['kodebarang'].value;
-    var b = document.forms['data']['namabarang'].value;
-    var c = document.forms['data']['hargabarang'].value;
-    var d = document.forms['data']['satuan'].value;
+    var a = document.forms['data']['kodepelanggan'].value;
+    var b = document.forms['data']['namapelanggan'].value;
+    var c = document.forms['data']['nohp'].value;
 
     var tabel = document.getElementById("table");
 
@@ -11,10 +10,8 @@ function inputdata() {
     var kol1 = baris.insertCell(0);
     var kol2 = baris.insertCell(1);
     var kol3 = baris.insertCell(2);
-    var kol4 = baris.insertCell(3);
 
     kol1.innerHTML = a;
     kol2.innerHTML = b;
     kol3.innerHTML = c;
-    kol4.innerHTML = d;
 }
